0a429acfd624ba36c3fc6ba50d81f27440f64ba5
Vinh Minh (Ryan) Dang <vinhminh020904@gmail.com>
2025-07-30 21:17:09 -0400

    fix-web

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+// import { RouterTestingModule } from '@angular/router/testing';
 import { AboutComponent } from './about.component';
+import { provideRouter } from '@angular/router';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -9,6 +10,9 @@ describe('AboutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AboutComponent],
+      providers:[
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
