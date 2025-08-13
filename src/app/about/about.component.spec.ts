0d117b3075f271c62c995881e6dbf166b1701a28
Vinh Minh (Ryan) Dang <vinhminh020904@gmail.com>
2025-08-12 23:31:00 -0400

    fix

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -4,14 +4,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
 import { provideRouter } from '@angular/router';
 
-//describe-it is a Jasmine syntax for writing test cases 
+//describe-it is a Jasmine syntax for writing test cases
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
   //set up before running test like import
   beforeEach(async () => {
-    await TestBed.configureTestingModule({ //configureTestingModule is for creating a env for test
+    await TestBed.configureTestingModule({
+      //configureTestingModule is for creating a env for test
       imports: [AboutComponent], //standalone comp.
       providers: [provideRouter([])], //mock/service whatever comp. needs
     }).compileComponents();
