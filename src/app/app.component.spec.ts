d277abfa038d8cd7b10d72d2180a0d508c94eaa0
Vinh Minh (Ryan) Dang <vinhminh020904@gmail.com>
2025-08-16 13:41:10 -0400

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -68,6 +68,7 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -75,7 +76,7 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HeaderComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
